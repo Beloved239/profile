33e31336434bc035758caffd3d5b8a5b9faaa164
Oluwatobi Adebanjo <108755660+Beloved239@users.noreply.github.com>
2023-02-15 04:17:30 +0100

    Add files via upload

---
+++ Oluwatobi Adebanjo Web/script/script.js
@@ -0,0 +1,7 @@
+/**Typing animation */
+var typed = new typed(".typing",{
+    Strings:["", "Web Desigener", "Web Developer", "Business Automator"],
+    typeSpeed:100,
+    Backspeed:60,
+    loop:true
+})
